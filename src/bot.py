e2f73dac21357e806c31d144f3512d9b6554ccdf
Elistellar <eliott.piccand@gmail.com>
2023-01-31 19:28:09 +0100

    2nd semestre update

--- src/bot.py
+++ src/bot.py
@@ -6,7 +6,6 @@ from random import choice, shuffle
 
 from discord import Activity, ActivityType, Embed, Intents, Interaction
 from discord.ext.commands import Bot
-from discord.ext import tasks
 
 from .config import Config
 from .database import Database
@@ -20,15 +19,15 @@ bot = Bot(
 )
 
 lunch_times = (
-    (time(hour=12, minute=33), time(hour=18, minute=36)), # Monday
-    (time(hour=11, minute=36), time(hour=19, minute=12)),
+    (time(hour=12, minute=12), time(hour=18, minute=36)), # Monday
+    (time(hour=12, minute=30), time(hour=19, minute=12)),
     (time(hour=12, minute=3 ), time(hour=19, minute=6 )), # ...
     (time(hour=11, minute=18), time(hour=19, minute=4 )),
     (time(hour=12, minute=12), time(hour=18, minute=25))  # Friday
 )
 
 night_time = (
-    time(hour=23, minute=30),
+    time(hour=23, minute=00),
     time(hour=6, minute=00),
 )
 
@@ -43,9 +42,6 @@ night_sentences = (
     "Gros morfal, attend encore quelques heures !",
     "A cette heure là ? Il serait temps d'aller dormir."
 )
-
-cooldown = timedelta(minutes=20)
-last_exec = datetime.now() - cooldown
 
 # events
 @bot.event
@@ -83,84 +79,77 @@ async def help(interaction: Interaction):
     ))
 
 # Secret Santa
-@bot.tree.command(name="join", description="Participe au Secret Santa !")
-async def join(interaction: Interaction):
+# @bot.tree.command(name="join", description="Participe au Secret Santa !")
+# async def join(interaction: Interaction):
     
-    current_users = Database.select("users", ["user_id"])
-    for user in current_users:
-        if user["user_id"] == interaction.user.id:
-            await interaction.response.send_message("Tu participe déjà au Secret Santa", ephemeral=True)
-            return
+#     current_users = Database.select("users", ["user_id"])
+#     for user in current_users:
+#         if user["user_id"] == interaction.user.id:
+#             await interaction.response.send_message("Tu participe déjà au Secret Santa", ephemeral=True)
+#             return
     
-    Database.insert("users", {
-        "user_id": interaction.user.id,
-    })
-    await interaction.response.send_message("Ta participation a bien été prise en compte !", ephemeral=True)
+#     Database.insert("users", {
+#         "user_id": interaction.user.id,
+#     })
+#     await interaction.response.send_message("Ta participation a bien été prise en compte !", ephemeral=True)
 
-@bot.tree.command(name="leave", description="Supprime ta participation au Secret Santa")
-async def leave(interaction: Interaction):
-    current_users = Database.select("users", ["user_id"])
-    for user in current_users:
-        if user["user_id"] == interaction.user.id:
-            Database.delete("users", where=f"user_id='{interaction.user.id}'")
-            break
+# @bot.tree.command(name="leave", description="Supprime ta participation au Secret Santa")
+# async def leave(interaction: Interaction):
+#     current_users = Database.select("users", ["user_id"])
+#     for user in current_users:
+#         if user["user_id"] == interaction.user.id:
+#             Database.delete("users", where=f"user_id='{interaction.user.id}'")
+#             break
 
-    await interaction.response.send_message("Ta participation a bien été supprimée", ephemeral=True)
+#     await interaction.response.send_message("Ta participation a bien été supprimée", ephemeral=True)
 
-@bot.tree.command(name="list", description="Affiche la liste des participants")
-async def list_users(interaction: Interaction):
+# @bot.tree.command(name="list", description="Affiche la liste des participants")
+# async def list_users(interaction: Interaction):
         
-    current_users = Database.select("users", ["user_id"])
+#     current_users = Database.select("users", ["user_id"])
     
-    text = "Voici la liste des participants actuels :"    
-    for user in current_users:
-        text += f"\n\t-<@{user['user_id']}>"
+#     text = "Voici la liste des participants actuels :"    
+#     for user in current_users:
+#         text += f"\n\t-<@{user['user_id']}>"
         
-    await interaction.response.send_message(text, ephemeral=True)
+#     await interaction.response.send_message(text, ephemeral=True)
 
-@bot.tree.command(name="roll", description="Lance le tirage au sort")
-async def roll(interaction: Interaction):
-    Database.delete("pairs")
+# @bot.tree.command(name="roll", description="Lance le tirage au sort")
+# async def roll(interaction: Interaction):
+#     Database.delete("pairs")
     
-    current_users = Database.select("users", ["user_id"])
+#     current_users = Database.select("users", ["user_id"])
     
-    from_ids = [u["user_id"] for u in current_users]
-    shuffle(from_ids)
+#     from_ids = [u["user_id"] for u in current_users]
+#     shuffle(from_ids)
     
-    to_ids = from_ids[1:] + [from_ids[0]]
+#     to_ids = from_ids[1:] + [from_ids[0]]
     
-    for from_id, to_id in zip(from_ids, to_ids):
-        Database.insert("pairs", {
-            "from_id": from_id,
-            "to_id": to_id,
-        })
+#     for from_id, to_id in zip(from_ids, to_ids):
+#         Database.insert("pairs", {
+#             "from_id": from_id,
+#             "to_id": to_id,
+#         })
         
-    await interaction.response.send_message("Les dés sont jetés !\nVous pouvez dès à présent voir la personne à que vous devez offrir un cadeau avec la commande /secret")
+#     await interaction.response.send_message("Les dés sont jetés !\nVous pouvez dès à présent voir la personne à que vous devez offrir un cadeau avec la commande /secret")
 
-@bot.tree.command(name="secret", description="Affiche le nom de la personne à qui tu vas offrir un cadeau")
-async def secret(interaction: Interaction):
-    if Database.select("pairs") == []:
-        await interaction.response.send_message("Le tirage n'a pas encore été effectué.", ephemeral=True)
-        return
+# @bot.tree.command(name="secret", description="Affiche le nom de la personne à qui tu vas offrir un cadeau")
+# async def secret(interaction: Interaction):
+#     if Database.select("pairs") == []:
+#         await interaction.response.send_message("Le tirage n'a pas encore été effectué.", ephemeral=True)
+#         return
     
-    data = Database.select("pairs", ["to_id"], where=f"from_id='{interaction.user.id}'")
-    await interaction.response.send_message(f"La personne qui t'a été attribuée est : <@{data[0]['to_id']}>", ephemeral=True)
+#     data = Database.select("pairs", ["to_id"], where=f"from_id='{interaction.user.id}'")
+#     await interaction.response.send_message(f"La personne qui t'a été attribuée est : <@{data[0]['to_id']}>", ephemeral=True)
 
 # /manger
 async def night_time_response(interaction: Interaction):
     await interaction.response.send_message(choice(night_sentences))
 
 @bot.tree.command(name="manger", description="Affiche l'heure du prochain repas")
-async def lunch(interaction: Interaction):
-    global last_exec, cooldown
-    
+async def lunch(interaction: Interaction):    
     now = datetime.now()
-    
-    if now < last_exec + cooldown:
-        return await interaction.response.send_message("La commande a déjà été executée un peu plus tôt, regarde juste au dessus.", ephemeral=True)
-    
     lunch, dinner = lunch_times[now.weekday()]
-    
     
     if now.time() < lunch:
         if now.time() < night_time[1]:
@@ -197,15 +186,3 @@ async def lunch(interaction: Interaction):
         )
     
     await interaction.response.send_message(choice(lunch_sentences).format(ts=int(date.timestamp())))
-    last_exec = now
-
-@tasks.loop(minutes=1)
-async def reset_cooldown():
-    global last_exec, cooldown
-    
-    now = datetime.now()
-    lunch, dinner = lunch_times[now.weekday()]
-    
-    if now - timedelta(minutes=1) <= lunch <= now \
-    or now - timedelta(minutes=1) <= dinner <= now:
-        last_exec = now - cooldown
